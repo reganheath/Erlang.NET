5c1d8704fd62e899c900f551bddbe119727cfcb3
Regan Heath <reganmheath@gmail.com>
2020-06-14 19:15:43 +0100

    Initial import and restructure

---
+++ premake5.lua
@@ -0,0 +1,91 @@
+-- premake5.lua
+defines { "TRACE" }
+
+workspace "Erlang.NET"
+    configurations { "Debug", "Release" }
+    platforms { "AnyCPU" }
+
+project "test"
+    location "src/test"
+    kind "ConsoleApp"
+    language "C#"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{prj.name}"
+
+    files { "src/test/**.cs", "src/test/App.Config" }
+    links { "System", "System.Configuration", "deps/log4net", "Erlang.NET" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+project "Erlang.NET"
+    location "src/Erlang.NET"
+    kind "SharedLib"
+    language "C#"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{prj.name}"
+
+    files { "src/Erlang.NET/**.cs", "src/Erlang.NET/App.Config" }
+    links { "System", "System.Configuration", "deps/log4net" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+project "echo"
+    location "src/echo"
+    kind "ConsoleApp"
+    language "C#"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{prj.name}"
+
+    files { "src/echo/**.cs", "src/echo/App.Config" }
+    links { "System", "System.Configuration", "deps/log4net", "Erlang.NET" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+project "epmd"
+    location "src/epmd"
+    kind "ConsoleApp"
+    language "C#"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{prj.name}"
+
+    files { "src/epmd/**.cs", "src/epmd/App.Config" }
+    links { "System", "System.Configuration", "System.Configuration.Install", "System.ServiceProcess", "deps/log4net", "Erlang.NET" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+project "ping"
+    location "src/ping"
+    kind "ConsoleApp"
+    language "C#"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{prj.name}"
+
+    files { "src/ping/**.cs", "src/ping/App.Config" }
+    links { "System", "System.Configuration", "deps/log4net", "Erlang.NET" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
---
+++ src/test/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using Erlang.NET;
+using log4net;
+using log4net.Config;
+
+namespace Erlang.NET.Test
+{
+    public class Echo
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        static Echo()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        public static void Main(string[] args)
+        {
+            OtpNode self = new OtpNode("vaplug", "edsrv_cookie");
+            OtpMbox mbox = self.createMbox("test", true);
+            OtpErlangTuple tuple = new OtpErlangTuple(new OtpErlangObject[2]
+            {
+                mbox.Self,
+                new OtpErlangAtom("hello, world")
+            });
+            mbox.send("echo", "edsrv@GAMING", tuple);
+        }
+    }
+}
